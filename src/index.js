de4068789d24b67ded54fdf68df2bfa97cd4b94b
Abdur Rehman <abdur4082@gmail.com>
2021-07-25 15:52:44 +0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+import { createStore } from "redux";
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
+  }
+};
+const store = createStore(counter);
+console.log(store.getState());
+const render = () => {
+  document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+document.addEventListener("click", () => {
+  store.dispatch({ type: "INCREMENT" });
+});
